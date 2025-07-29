96de7499c88e18466b80c92b634d77a98ca2b6e8
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2025-07-29 13:13:05 +0700

    .

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password='',
+        password='RootPass123!',
         database='db_maintenance_alat'
     )
